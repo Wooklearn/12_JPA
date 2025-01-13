e74f31a217a6a947bb8be559d56606fc59ab985d
Yongwook <kyw9885@gmail.com>
2025-01-13 10:19:29 +0900

    [0112] JPA-CRUD

---
+++ 01_lecture-sourece/chap06-spring-data-jpa-crud/src/main/java/com/ohgiraffers/springdatajpa/menu/entity/Menu.java
@@ -0,0 +1,34 @@
+package com.ohgiraffers.springdatajpa.menu.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "tbl_menu")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Menu {
+
+    @Id
+    @Column(name = "menu_code")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int menuCode;
+
+    @Column(name = "menu_name")
+    private String menuName;
+
+    @Column(name = "menu_price")
+    private int menuPrice;
+
+    @Column(name = "category_code")
+    private int categoryCode;
+
+    @Column(name = "orederable_status")
+    private String orderableStatus;
+
+}
